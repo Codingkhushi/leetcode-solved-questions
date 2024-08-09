f4d464412867f4c8670c3c4657a3c3195bc4ee51
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2024-08-09 18:56:14 +0530

    Time: 1 ms (99.78%), Space: 52.9 MB (57.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     int val = map.getOrDefault(nums[i],0);
+        //     map.put(nums[i],val+1);
+        // }
+        // for(Map.Entry<Integer,Integer> m : map.entrySet()){
+        //     if(m.getValue() > (nums.length /2)){
+        //         return m.getKey();
+        //     }
+        // }
+        // return -1;
+
+
+        int count = 0;
+        int el=0;
         for(int i=0;i<nums.length;i++){
-            int val = map.getOrDefault(nums[i],0);
-            map.put(nums[i],val+1);
-        }
-        for(Map.Entry<Integer,Integer> m : map.entrySet()){
-            if(m.getValue() > (nums.length /2)){
-                return m.getKey();
+            if(count == 0){
+                count = 1;
+                el = nums[i];
+            }else if(nums[i] == el){
+                count++;
+            }else{
+                count--;
             }
         }
+        int c = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == el) c++;
+        }    
+        if(c > (nums.length/2)) return el;
         return -1;
     }
 }
