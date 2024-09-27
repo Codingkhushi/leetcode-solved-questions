f102dae2f8e7f82fffc77875e80def73e1b403fb
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2024-09-27 17:54:11 +0530

    Update 0021-merge-two-sorted-lists.java

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,6 +8,14 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+Unwinding the Recursive Calls:
+
+	•	The fifth recursive call returns list1 = [4], which is set as the next node of list2 = [4] in the fourth recursive call. So, the merged list is [4 -> 4].
+	•	The fourth recursive call returns list2 = [3 -> 4 -> 4].
+	•	The third recursive call returns list1 = [2 -> 3 -> 4 -> 4].
+	•	The second recursive call returns list1 = [1 -> 2 -> 3 -> 4 -> 4].
+	•	The first recursive call returns list2 = [1 -> 1 -> 2 -> 3 -> 4 -> 4].
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 !=null && list2 != null){
