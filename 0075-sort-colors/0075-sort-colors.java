296625ab181155fd1d865a76ba2224fd917b9c40
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2026-02-16 11:02:29 +0530

    Refactor sortColors method to use count variables

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public void sortColors(int[] nums) {
-        int c0 = 0,c1 = 0,c2 = 0;
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+
         for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                c0++;
-            }else if(nums[i] == 1){
-                c1++;
-            }else{
-                c2++;
-            }
+            if(nums[i] == 0) countZero++;
+            else if(nums[i] == 1) countOne++;
+            else countTwo++;
         }
-        for(int i=0;i<c0;i++){
-            nums[i] = 0;
+        int idx = 0;
+        for(int i=0;i<countZero;i++){
+            nums[idx++] = 0;
         }
-        for(int i=c0;i<c0+c1;i++){
-            nums[i] = 1;
+        for(int i=0;i<countOne;i++){
+            nums[idx++] = 1;
         }
-        for(int i=c0+c1;i<nums.length;i++){
-            nums[i] = 2;
+        for(int i=0;i<countTwo;i++){
+            nums[idx++] = 2;
         }
+        
     }
 }
