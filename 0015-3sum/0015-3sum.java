95547f005b7f1ca1560b33d3946ced3f98b7f6dc
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2026-02-18 12:13:05 +0530

    Refactor threeSum method for clarity and efficiency

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,25 +3,24 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
-            if(i != 0 && nums[i]==nums[i-1]) continue;
-            int j = i+1,k = nums.length-1;
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.length-1;
             while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0){
-                    j++;
-                }else if(sum > 0){
-                    k--;
-                }else{
-                    List<Integer>temp = Arrays.asList(nums[i],nums[j],nums[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
-                }
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < 0) j++;
+                else if(sum > 0) k--;
+                else{
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++; k--;
 
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                }
             }
+            
+            
         }
         return ans;
+        
     }
 }
