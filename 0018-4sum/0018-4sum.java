322ba128d9f25fc869056b2f61fff261057ca169
Codingkhushi <98373209+Codingkhushi@users.noreply.github.com>
2026-02-19 11:02:48 +0530

    Refactor variable names in fourSum method
    Refactor fourSum method to use 'arr' instead of 'nums' for parameter and variable names.

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,4 +1,39 @@
 class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1;j<n;){
+                int k = j+1; int l = n-1;
+                while(k < l){
+                    long sum = (long)nums[i]+ (long)nums[j]+ (long)nums[k]+ (long)nums[l];
+                    if(sum < target){
+                        k++;
+                    }else if(sum > target){
+                        l--;
+                    }
+                    else{
+                        res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        k++;l--;
+
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                    }
+    
+                }
+                j++;
+                while(j<n && nums[j] == nums[j-1]) j++;
+                //after this loop j++ will be done and will go to for loop then again j++ and we don't want that that's why above line 
+            }
+        }
+        return res;
+        
+    }
+}
+-----------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
     public List<List<Integer>> fourSum(int[] arr, int target) {
         List<List<Integer>> res = new ArrayList<>();
         int n = arr.length;
